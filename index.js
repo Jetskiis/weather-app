3dd96d4bfeef912481223a3177c065cd8c62ff12
gene <yegene60@gmail.com>
2021-11-24 22:09:01 -0500

    Add default homepage (see more)
    Started working on displaying the data for the upcoming days in the weather app, ignore all functions for now

--- index.js
+++ index.js
@@ -18,13 +18,18 @@ const fahrenheit = document.querySelector("#fahrenheit");
 const celsius = document.querySelector("#celsius");
 const button = document.querySelector("#search-button");
 
-fahrenheit.classList.add("selected");
-fahrenheit.addEventListener("click", toggleSelected);
-celsius.addEventListener("click", toggleSelected);
-document.addEventListener("keypress", (e) => {
-  if (e.code == "Enter") submitForm();
-});
-button.addEventListener("click", submitForm);
+//default settings for the page
+(function() {
+  fahrenheit.classList.add("selected");
+  fahrenheit.addEventListener("click", toggleSelected);
+  celsius.addEventListener("click", toggleSelected);
+  document.addEventListener("keypress", (e) => {
+    if (e.code == "Enter") submitForm();
+  });
+  button.addEventListener("click", submitForm);
+
+  getData("New York").then((response)=>parseData(response));
+})();
 
 function toggleSelected(e) {
   let clicked = e.target.getAttribute("id");
@@ -61,16 +66,19 @@ async function submitForm() {
 async function getData(city) {
   try {
     let selected = document.querySelector(".selected").getAttribute("id");
-    const response = await fetch(
+
+    //first api request for today data
+    let response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${
         selected == "fahrenheit" ? "imperial" : "metric"
       }&appid=99d2c5e24128552205c7bd4bdccb30b5`
     );
-    const fetchData = await response.json();
-    const info = fetchData.main;
-    const type = fetchData.weather;
+    let fetchData = await response.json();
+    let info = fetchData.main;
+    let type = fetchData.weather;
+    let coord = [fetchData.coord.lon, fetchData.coord.lat];
 
-    let returnData = {
+    let todayData = {
       city,
       "feels-like": info.feels_like,
       humidity: info.humidity,
@@ -81,29 +89,56 @@ async function getData(city) {
       description: type[0].description,
     };
 
+    //second api request for upcoming data
+    response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${coord[1]}&lon=${
+        coord[0]
+      }&exclude=current,minutely,hourly,alerts&units=${
+        selected == "fahrenheit" ? "imperial" : "metric"
+      }&appid=99d2c5e24128552205c7bd4bdccb30b5`
+    );
+    fetchData = await response.json();
+    info = fetchData.daily;
+
+    let upcomingData = {
+      dayZero: { highLow: info[0].temp, description: info[0].weather[0] },
+      dayOne: { highLow: info[1].temp, description: info[1].weather[0] },
+      dayTwo: { highLow: info[2].temp, description: info[2].weather[0] },
+      dayThree: { highLow: info[3].temp, description: info[3].weather[0] },
+      dayFour: { highLow: info[4].temp, description: info[4].weather[0] },
+      dayFive: { highLow: info[5].temp, description: info[5].weather[0] },
+    };
+
+    let returnData = [todayData, upcomingData];
     return returnData;
   } catch (err) {
+    console.log(err);
     return false;
   }
 }
 
-//goes through data and cleans it up
+//goes through data and parses/cleans it up to be rendered
 function parseData(data) {
-  let cityName = data.city
+  //data for today
+  let cityName = data[0].city
     .toLowerCase()
     .split(" ")
     .map((str) => str.charAt(0).toUpperCase() + str.substring(1))
     .join(" ");
-  let feelsLike = Math.floor(data["feels-like"]);
-  let humidity = data.humidity;
-  let currentTemp = Math.floor(data["current-temp"]);
-  let high = Math.floor(data.high);
-  let low = Math.floor(data.low);
-  let type = data.type;
-  let description = data.description;
+  let feelsLike = Math.floor(data[0]["feels-like"]);
+  let humidity = data[0].humidity;
+  let currentTemp = Math.floor(data[0]["current-temp"]);
+  let high = Math.floor(data[0].high);
+  let low = Math.floor(data[0].low);
+  let type = data[0].type;
+  let description = data[0].description;
   let image = "";
   let background = "";
 
+  //image array for upcoming
+  let upcomingImages = [];
+
+  //image is the icon for today
   switch (type) {
     case "Clear":
       image = icons.Clear;
@@ -117,7 +152,7 @@ function parseData(data) {
       image = icons.Rain;
       background = "./img/rainy.jpg";
       break;
-    case "Snowing":
+    case "Snow":
       image = icons.Snowing;
       background = "./img/snowing.jpg";
       break;
@@ -154,11 +189,43 @@ function parseData(data) {
     background,
   };
 
-  let returnUpcomingData = {};
+  let returnUpcomingData = {
+    "day-one": [
+      Math.floor(data[1].dayZero.highLow.max),
+      Math.floor(data[1].dayZero.highLow.min),
+      data[1].dayZero.description.main,
+    ],
+    "day-two": [
+      Math.floor(data[1].dayOne.highLow.max),
+      Math.floor(data[1].dayOne.highLow.min),
+      data[1].dayOne.description.main,
+    ],
+    "day-three": [
+      Math.floor(data[1].dayTwo.highLow.max),
+      Math.floor(data[1].dayTwo.highLow.min),
+      data[1].dayTwo.description.main,
+    ],
+    "day-four": [
+      Math.floor(data[1].dayThree.highLow.max),
+      Math.floor(data[1].dayThree.highLow.min),
+      data[1].dayThree.description.main,
+    ],
+    "day-five": [
+      Math.floor(data[1].dayFour.highLow.max),
+      Math.floor(data[1].dayFour.highLow.min),
+      data[1].dayFour.description.main,
+    ],
+    "day-six": [
+      Math.floor(data[1].dayFive.highLow.max),
+      Math.floor(data[1].dayFive.highLow.min),
+      data[1].dayFive.description.main,
+    ],
+  };
 
   renderData(returnTodayData, "today");
+  renderData(returnUpcomingData, "upcoming");
 
-  //console.log(returnData);
+  //console.log(Object.keys(returnUpcomingData));
 }
 
 //displays data on site
@@ -174,14 +241,14 @@ function renderData(data, options) {
   const todayHighLow = todayInfo.querySelector(".high-low");
   const todayType = todayInfo.querySelector(".type");
 
-  const upcoming = document.querySelector(".upcoming");
+  /*   const upcoming = document.querySelector(".upcoming");
 
   const dayOne = document.querySelector("#day-one");
   const dayTwo = document.querySelector("#day-two");
   const dayThree = document.querySelector("#day-three");
   const dayFour = document.querySelector("#day-four");
   const dayFive = document.querySelector("#day-five");
-  const daySix = document.querySelector("#day-six");
+  const daySix = document.querySelector("#day-six"); */
 
   if (options == "today") {
     cityName.textContent = data.cityName;
@@ -197,5 +264,12 @@ function renderData(data, options) {
     todayType.textContent = data.description;
     root.style.setProperty("--bg", `url("${data.background}")`);
   } else if (options == "upcoming") {
+    for (key of Object.keys(data)) {
+      let upcoming = document.querySelector(`#${key}`);
+      let highLow = upcoming.querySelector(".high-low");
+      let type = upcoming.querySelector(".type");
+      highLow.textContent = `${data[key][0]}° / ${data[key][1]}°`;
+      type.textContent = data[key][2];
+    }
   }
 }
